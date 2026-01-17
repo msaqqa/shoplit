19a1ffa5944bc5f1c32bffa8d890d50333040443
masaqqa <ma.saqqa@gmail.com>
2026-01-17 14:51:03 +0200

    fix prisma client file after edit it

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -15,17 +15,7 @@ import { PrismaClient } from "@prisma/client";
 // }
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+  return new PrismaClient();
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
